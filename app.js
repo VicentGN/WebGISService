34f77cccb4e53159c885496436bd03ae31a4c937
VicentGN <vicentgarcian@gmail.com>
2022-03-18 18:18:41 +0100

    ADD: area and length

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 app.use(express.json());
-const { getBuffer, getCentroid } = require('./gisUtils');
+const { getBuffer, getCentroid, getArea, getLength } = require('./gisUtils');
 
 app.get('/', (req, res) => {
     res.send('Welcome to WebGISService!');
+});
+
+app.post('/area', function (req, res) {
+    if(req.body.geometry.type !== 'Polygon') {
+        res.sendStatus(400);
+    } else {
+        res.send(getArea(req));
+    }
 });
 
 app.post('/buffer', function(req, res) {
@@ -23,6 +31,15 @@ app.post('/centroid', function(req, res) {
         res.send(getCentroid(req));
     }
 });
+
+app.post('/length', function(req, res) {
+    if (req.body.geometry.type !== 'LineString') {
+        res.sendStatus(400);
+    } else {
+        res.send(getLength(req));
+    }
+});
+
 
 // eslint-disable-next-line no-undef
 var port = process.env.PORT || 3000;
--- gisUtils.js
+++ gisUtils.js
@@ -1,13 +1,27 @@
 const turf = require('@turf/turf');
 
-function getBuffer(req) {
+let getBuffer = req => {
     return turf.buffer(turf.point(req.body.geometry.coordinates), req.query.distance || 500, { units: req.query.units || 'meters' });
-}
+};
 
-function getCentroid(req) {
+let getCentroid = req => {
     return turf.centroid(turf.polygon(req.body.geometry.coordinates));
-}
+};
+
+let getArea = req => {
+    let result = {};
+    result.value = turf.area(turf.polygon(req.body.geometry.coordinates));
+    return result;
+};
+
+let getLength = req => {
+    let result = {};
+    result.value = turf.length(turf.lineString(req.body.geometry.coordinates));
+    return result;
+};
 
 
 module.exports.getBuffer = getBuffer;
 module.exports.getCentroid = getCentroid;
+module.exports.getArea = getArea;
+module.exports.getLength = getLength;
