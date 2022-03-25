6ec9cc58483a0adae9b19cadaf06f59817e37ac1
VicentGN <vicentgarcian@gmail.com>
2022-03-25 23:11:59 +0100

    UPDATE gisUtils

--- gisUtils.js
+++ gisUtils.js
@@ -18,20 +18,21 @@ let getDistance = req => {
     let from = req.body.features[0].geometry.coordinates;
     let to = req.body.features[1].geometry.coordinates;
     let result = {};
-    result.value = turf.distance(from, to, { units:  req.query.units || 'kilometers' });
+    result.value = turf.distance(from, to, { units: req.query.units || 'kilometers' });
     return result;
 };
 
 
 let getLength = req => {
     let result = {};
-    result.value = turf.length(turf.lineString(req.body.geometry.coordinates), { units:  req.query.units || 'kilometers' });
+    result.value = turf.length(turf.lineString(req.body.geometry.coordinates), { units: req.query.units || 'kilometers' });
     return result;
 };
 
-
-module.exports.getArea = getArea;
-module.exports.getBuffer = getBuffer;
-module.exports.getCentroid = getCentroid;
-module.exports.getDistance = getDistance;
-module.exports.getLength = getLength;
+module.exports = {
+    getArea,
+    getBuffer,
+    getCentroid,
+    getDistance,
+    getLength
+};
