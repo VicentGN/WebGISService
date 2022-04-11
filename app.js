0e12f4b6e6cc2465bf714e4d97f76b48d7eeb3b3
VicentGN <vicentgarcian@gmail.com>
2022-04-11 18:25:24 +0200

    ADD: geojson schema validation

--- app.js
+++ app.js
@@ -6,16 +6,25 @@ app.use(express.json());
 
 const yaml = require('yamljs');
 
+// Swagger
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = yaml.load('./swagger.yaml');
-
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+
+// Turf Tools
 const { getBuffer, getCentroid, getArea, getLength, getDistance } = require('./gisUtils');
+
+// GeoJSON validation
+const { isValidFeatureSchema } = require('./utils');
 
 // eslint max-len: ["error", { "code": 80 }]
 
 app.post('/area', function(req, res) {
+    let schemaValidation = isValidFeatureSchema(req);
+    if (!schemaValidation.status) {
+        res.status(400).send({ status_code: 400, message: 'The GeoJSON is not valid', details: schemaValidation.errors });
+    } else
     if (req.body.geometry.type !== 'Polygon') {
         res.sendStatus(400);
     } else {
@@ -24,7 +33,10 @@ app.post('/area', function(req, res) {
 });
 
 app.post('/buffer', function(req, res) {
-    if (req.body.geometry.type !== 'Point') {
+    let schemaValidation = isValidFeatureSchema(req);
+    if (!schemaValidation.status) {
+        res.status(400).send({ status_code: 400, message: 'The GeoJSON is not valid', details: schemaValidation.errors });
+    } else if (req.body.geometry.type !== 'Point') {
         res.sendStatus(400);
     } else {
         res.send(getBuffer(req));
@@ -32,13 +44,17 @@ app.post('/buffer', function(req, res) {
 });
 
 app.post('/centroid', function(req, res) {
-    if (req.body.geometry.type !== 'Polygon') {
+    let schemaValidation = isValidFeatureSchema(req);
+    if (!schemaValidation.status) {
+        res.status(400).send({ status_code: 400, message: 'The GeoJSON is not valid', details: schemaValidation.errors });
+    } else if (req.body.geometry.type !== 'Polygon') {
         res.sendStatus(400);
     } else {
         res.send(getCentroid(req));
     }
 });
 
+// TODO: Add validation for FeatureCollection Schema
 app.post('/distance', function(req, res) {
     if (req.body.type !== 'FeatureCollection' ||
         req.body.features.length !== 2) {
@@ -52,7 +68,10 @@ app.post('/distance', function(req, res) {
 });
 
 app.post('/length', function(req, res) {
-    if (req.body.geometry.type !== 'LineString') {
+    let schemaValidation = isValidFeatureSchema(req);
+    if (!schemaValidation.status) {
+        res.status(400).send({ status_code: 400, message: 'The GeoJSON is not valid', details: schemaValidation.errors });
+    } else if (req.body.geometry.type !== 'LineString') {
         res.sendStatus(400);
     } else {
         res.send(getLength(req));
