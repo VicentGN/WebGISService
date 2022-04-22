7f8f9b26798ab4250e9ac210848fedecd75b18b2
VicentGN <vicentgarcian@gmail.com>
2022-04-22 19:00:02 +0200

    UPDATE: gisUtils rewrited

--- gisUtils.js
+++ gisUtils.js
@@ -1,33 +1,33 @@
 const turf = require('@turf/turf');
 
-let getArea = req => {
+function getArea(req) {
     let result = {};
     result.value = turf.area(turf.polygon(req.body.geometry.coordinates));
     return result;
-};
+}
 
-let getBuffer = req => {
+function getBuffer(req) {
     return turf.buffer(turf.point(req.body.geometry.coordinates), req.query.distance || 500, { units: req.query.units || 'meters' });
-};
+}
 
-let getCentroid = req => {
+function getCentroid(req) {
     return turf.centroid(turf.polygon(req.body.geometry.coordinates));
-};
+}
 
-let getDistance = req => {
+function getDistance(req) {
     let from = req.body.features[0].geometry.coordinates;
     let to = req.body.features[1].geometry.coordinates;
     let result = {};
     result.value = turf.distance(from, to, { units: req.query.units || 'kilometers' });
     return result;
-};
+}
 
 
-let getLength = req => {
+function getLength(req) {
     let result = {};
     result.value = turf.length(turf.lineString(req.body.geometry.coordinates), { units: req.query.units || 'kilometers' });
     return result;
-};
+}
 
 module.exports = {
     getArea,
