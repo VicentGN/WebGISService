f2242ae5de4fc534eea7aba3d4e7a10a51fbb2cd
VicentGN <vicentgarcian@gmail.com>
2022-12-18 14:43:53 +0100

    Refactor validation

---
+++ middleware/geometryChecker.js
@@ -0,0 +1,29 @@
+const geometryChecker = {
+
+    isPoint: (req, res, next) => {
+        req.body.geometry.type === 'Point' ? req.geometryValidation = true : req.geometryValidation = false;
+        next();
+    },
+
+    isLineString: (req, res, next) => {
+        req.body.geometry.type === 'LineString' ? req.geometryValidation = true : req.geometryValidation = false;
+        next();
+    },
+
+    isPolygon: (req, res, next) => {
+        req.body.geometry.type === 'Polygon' ? req.geometryValidation = true : req.geometryValidation = false;
+        next();
+    },
+
+    isMultiPoint: (req, res, next) => {
+        req.geometryValidation = true;
+        let counter = 0;
+        while (req.geometryValidation && counter < req.body.features.length) {
+            req.body.features[counter].geometry.type !== 'Point' ? req.geometryValidation = !req.geometryValidation : false;
+            counter += 1;
+        }
+        next();
+    }
+};
+
+module.exports = { geometryChecker };
---
+++ utils/responses.js
@@ -0,0 +1,5 @@
+let response200Error = { status_code: 200, message: 'The GeoJSON is not valid' };
+
+module.exports = {
+    response200Error
+};
