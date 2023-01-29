8721f05558b0e7022a5a0b1aaf00d1c2ac23814a
VicentGN <vicentgarcian@gmail.com>
2023-01-29 16:29:26 +0100

    REFACTOR: geometry checker

--- app.js
+++ app.js
@@ -1,56 +1,89 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 
 const app = express();
 app.use(express.json());
 
-const yaml = require('yamljs');
+const yaml = require("yamljs");
 
 // Swagger
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = yaml.load('./openApi.yaml');
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = yaml.load("./openApi.yaml");
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Turf Tools
-const { getBuffer, getCentroid, getArea, getLength, getDistance } = require('./spatialTools');
+const {
+	getBuffer,
+	getCentroid,
+	getArea,
+	getLength,
+	getDistance,
+} = require("./spatialTools");
 
 // GeoJSON validation
-const { response200Error } = require('./utils/responses');
-const { schemaValidation } = require('./middleware/schemaValidation');
-const { geometryChecker } = require('./middleware/geometryChecker');
+const {response200Error} = require("./utils/responses");
+const {schemaValidation} = require("./middleware/schemaValidation");
+const {geometryChecker} = require("./middleware/geometryChecker");
 
 // eslint max-len: ["error", { "code": 80 }]
 
-app.get('/', function(req, res) {
-    res.redirect('/docs');
+app.get("/", function (req, res) {
+	res.redirect("/docs");
 });
 
-app.post('/area', [schemaValidation.feature, geometryChecker.isPolygon], function(req, res) {
-    req.schemaValidation && req.geometryValidation ? res.status(200).send(getArea(req)) : res.status(200).send(response200Error);
-});
+app.post(
+	"/area",
+	[schemaValidation.feature, geometryChecker.isPolygon],
+	function (req, res) {
+		req.schemaValidation && req.geometryValidation
+			? res.status(200).send(getArea(req))
+			: res.status(200).send(response200Error);
+	}
+);
 
-app.post('/buffer', [schemaValidation.feature, geometryChecker.isPoint], function(req, res) {
-    req.schemaValidation && req.geometryValidation ? res.status(200).send(getBuffer(req)) : res.status(200).send(response200Error);
-});
+app.post(
+	"/buffer",
+	[schemaValidation.feature, geometryChecker.isPoint],
+	function (req, res) {
+		req.schemaValidation && req.geometryValidation
+			? res.status(200).send(getBuffer(req))
+			: res.status(200).send(response200Error);
+	}
+);
 
-app.post('/centroid', [schemaValidation.feature, geometryChecker.isPolygon], function(req, res) {
-    req.schemaValidation && req.geometryValidation ? res.status(200).send(getCentroid(req)) : res.status(200).send(response200Error);
-});
+app.post(
+	"/centroid",
+	[schemaValidation.feature, geometryChecker.isPolygon],
+	function (req, res) {
+		req.schemaValidation && req.geometryValidation
+			? res.status(200).send(getCentroid(req))
+			: res.status(200).send(response200Error);
+	}
+);
 
-app.post('/distance', [schemaValidation.featureCollection, geometryChecker.isMultiPoint], function(req, res) {
-    req.schemaValidation && req.geometryValidation ? res.status(200).send(getDistance(req)) : res.status(200).send(response200Error);
+app.post(
+	"/distance",
+	[schemaValidation.featureCollection, geometryChecker.isMultiPoint],
+	function (req, res) {
+		req.schemaValidation && req.geometryValidation
+			? res.status(200).send(getDistance(req))
+			: res.status(200).send(response200Error);
+	}
+);
 
-});
-
-app.post('/length', [schemaValidation.feature, geometryChecker.isLineString], function(req, res) {
-    req.schemaValidation && req.geometryValidation ? res.status(200).send(getLength(req)) : res.status(200).send(response200Error);
-});
-
+app.post(
+	"/length",
+	[schemaValidation.feature, geometryChecker.isLineString],
+	function (req, res) {
+		req.schemaValidation && req.geometryValidation
+			? res.status(200).send(getLength(req))
+			: res.status(200).send(response200Error);
+	}
+);
 
 // eslint-disable-next-line no-undef
 var port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });
--- middleware/geometryChecker.js
+++ middleware/geometryChecker.js
@@ -1,29 +1,36 @@
 const geometryChecker = {
+	isPoint: (req, res, next) => {
+		req.body.geometry.type === "Point"
+			? (req.geometryValidation = true)
+			: (req.geometryValidation = false);
+		next();
+	},
 
-    isPoint: (req, res, next) => {
-        req.body.geometry.type === 'Point' ? req.geometryValidation = true : req.geometryValidation = false;
-        next();
-    },
+	isLineString: (req, res, next) => {
+		req.body.geometry.type === "LineString"
+			? (req.geometryValidation = true)
+			: (req.geometryValidation = false);
+		next();
+	},
 
-    isLineString: (req, res, next) => {
-        req.body.geometry.type === 'LineString' ? req.geometryValidation = true : req.geometryValidation = false;
-        next();
-    },
+	isPolygon: (req, res, next) => {
+		req.body.geometry.type === "Polygon"
+			? (req.geometryValidation = true)
+			: (req.geometryValidation = false);
+		next();
+	},
 
-    isPolygon: (req, res, next) => {
-        req.body.geometry.type === 'Polygon' ? req.geometryValidation = true : req.geometryValidation = false;
-        next();
-    },
-
-    isMultiPoint: (req, res, next) => {
-        req.geometryValidation = true;
-        let counter = 0;
-        while (req.geometryValidation && counter < req.body.features.length) {
-            req.body.features[counter].geometry.type !== 'Point' ? req.geometryValidation = !req.geometryValidation : false;
-            counter += 1;
-        }
-        next();
-    }
+	isMultiPoint: (req, res, next) => {
+		req.geometryValidation = true;
+		let counter = 0;
+		while (req.geometryValidation && counter < req.body.features.length) {
+			req.body.features[counter].geometry.type !== "Point"
+				? (req.geometryValidation = !req.geometryValidation)
+				: false;
+			counter += 1;
+		}
+		next();
+	},
 };
 
 module.exports = { geometryChecker };
